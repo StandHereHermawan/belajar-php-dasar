5cf71a5b1b0a7a54e88336989f98ee3408763326
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-05 16:26:17 +0700

    Constant, PHP Dasar

---
+++ 8-constant/constant.php
@@ -0,0 +1,12 @@
+<?php
+// Constant di PHP
+define('AUTHOR', 'Arief Karditya Hermawan');
+define('APP_VERSION', 100);
+
+echo "Author\t\t: ";
+echo AUTHOR;
+echo "\n\n";
+
+echo "App Version\t: ";
+echo APP_VERSION;
+echo "\n\n";

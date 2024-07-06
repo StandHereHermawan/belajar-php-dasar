9261934724c2acf8fc5f0a05a843a2a347e34650
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-06 21:35:51 +0700

    Operator Logika, PHP Dasar

---
+++ 14-operator-logika/logika.php
@@ -0,0 +1,15 @@
+<?php
+echo "Dan\n";
+var_dump(true && true);
+var_dump(true and true);
+var_dump(true && false);
+echo"\nOr\n";
+var_dump(true || false);
+var_dump(true or false);
+echo"\nXor\n";
+var_dump(true xor false);
+var_dump(false xor true);
+var_dump(true xor true);
+var_dump(false xor false);
+echo"\nNot\n";
+var_dump(!true);

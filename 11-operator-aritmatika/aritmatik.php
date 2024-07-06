41630d88cfb56dfb0bf0081568b668c33c6dd86a
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-06 20:56:51 +0700

    Operator Aritmatika, PHP Dasar

---
+++ 11-operator-aritmatika/aritmatik.php
@@ -0,0 +1,12 @@
+<?php
+
+$resultSum = 10 + 10;
+echo "var_dump( \$result ) = ";
+var_dump($resultSum);
+echo "\n\n";
+
+// Operasi Sisa Bagi.
+$resultMod = 100 % 3;
+echo "var_dump( \$resultMod ) = ";
+var_dump($resultMod);
+echo "\n\n";

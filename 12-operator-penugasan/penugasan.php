94fc0dc0facb5962e886f169bbba063a6f77e071
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-06 21:07:30 +0700

    Operator Penugasan, PHP Dasar

---
+++ 12-operator-penugasan/penugasan.php
@@ -0,0 +1,13 @@
+<?php
+
+$total = 0;
+
+$burgerBangorSultan = 61999;
+$burgerBangorNingrat = 51999;
+$burgerBangorJuragan = 21999;
+
+$total += $burgerBangorSultan; // = $total = $total + $burgerBangorSultan 
+$total += $burgerBangorNingrat; // = $total = $total + burgerBangorNingrat
+$total += $burgerBangorJuragan; // = $total = $total + $burgerBangorJuragan
+
+var_dump($total);

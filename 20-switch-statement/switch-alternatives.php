8a7898cf6fd16f8bf6e7e6720ce7ee3d9186bcd7
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-07 19:34:09 +0700

    Switch Statement, PHP Dasar

---
+++ 20-switch-statement/switch-alternatives.php
@@ -0,0 +1,27 @@
+<?php
+
+$nilai = "B";
+
+switch ($nilai):
+    case "A":
+        echo "Nilai anda A. Anda Lulus dengan Sangat Baik." . PHP_EOL;
+        break;
+    case "AB":
+        echo "Nilai anda AB. Anda Lulus dengan Sangat Baik." . PHP_EOL;
+        break;
+    case "B":
+        echo "Nilai anda B. Anda Lulus dengan Baik." . PHP_EOL;
+        break;
+    case "BC":
+        echo "Nilai anda BC. Anda Lulus dengan Baik." . PHP_EOL;
+        break;
+    case "C":
+        echo "Nilai anda C. Anda Lulus." . PHP_EOL;
+        break;
+    case "D":
+        echo "Nilai anda D. Anda Tidak Lulus." . PHP_EOL;
+        break;
+    default:
+        echo "Nilai anda E. Anda Tidak Lulus dengan Sangat Baik." . PHP_EOL;
+        break;
+endswitch;

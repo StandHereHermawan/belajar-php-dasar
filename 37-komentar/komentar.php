09ceda4cb68dca0d25b08dc63bb9c4580ac2e807
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-10 15:31:57 +0700

    Komentar, PHP Dasar

---
+++ 37-komentar/komentar.php
@@ -0,0 +1,21 @@
+<?php
+
+/* Komentar Multi Baris Cara 1.
+Komentar Multi Baris, bisa untuk lebih dari 1 baris.
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe assumenda totam eaque nihil quis, corrupti consequuntur unde, ratione nobis quasi quidem cupiditate officiis consequatur voluptates tempore distinctio dolores perspiciatis culpa?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil vitae quibusdam molestias iure quos quo. Officiis doloribus ullam numquam perspiciatis earum eos aliquid voluptatem necessitatibus expedita veritatis, totam adipisci inventore.
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam sunt repellendus magni id totam dolores adipisci molestiae quibusdam accusamus earum, iste perspiciatis rem itaque, quisquam quos fugit quia iure cumque.
+*/
+
+/** Komentar Multi Baris Cara 2.
+ * 
+ * Ini adalah function untuk menampilkan tulisan "Test." di konsole.    
+ */
+function test()
+{
+    // Komentar 1 baris.
+    echo "Test." . PHP_EOL;
+    # Komentar 1 Baris Bash Style.
+}
+
+test();
